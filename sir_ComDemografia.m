0f9e46dad58293d36416706436787a64fb3303fb
evandromarquesone <163877605+evandromarquesone@users.noreply.github.com>
2024-06-04 15:13:37 -0300

    Update sir_ComDemografia.m

--- sir_ComDemografia.m
+++ sir_ComDemografia.m
@@ -1,23 +1,28 @@
+% Material Complementar do Livro: 
+% T√≥picos Matem√°ticos Aplicados na Modelagem em Epidemiologia - Estudos Transversais
+
+% = Outros Materiais est√£o dispon√≠veis em https://linktr.ee/livroepidmat =
+
 function dy = sir_ComDemografia(t,y)
-% ============== Declarando o valor de cada Par‚metro ==================
-% ===== Esses s„o os valores que ser„o considerados no modelo ==========
-n=50000; %50 mil indivÌduos
+% ============== Declarando o valor de cada Par√¢metro ==================
+% ===== Esses s√£o os valores que ser√£o considerados no modelo ==========
+n=50000; %50 mil indiv√≠duos
 beta=0.1/n; %taxa per-capita de 10% dos encontros tornarem-se contaminados)
 mu=1/(80*365); %(expectativa de 80 anos de vida)
 %mu=0;
 %d=0;
-nu=1/60; %(a taxa È 1 dividido pelo tempo em que se fica infectado)
+nu=1/60; %(a taxa √© 1 dividido pelo tempo em que se fica infectado)
 
-%alpha=0.001; %( mortalidade pela doenÁa de 1 a cada 1000)
+%alpha=0.001; %( mortalidade pela doen√ßa de 1 a cada 1000)
 alpha=0;
-%% ================ EquaÁıes do Modelo =================================
+%% ================ Equa√ß√µes do Modelo =================================
 dy = zeros(3,1);
 
-% %  ======= vers„o com demografia e mortalidade induzida pela doenÁa ===
-dy(1)=mu*n-beta*y(1)*y(2)-mu*y(1);              %SuscetÌveis      
+% %  ======= vers√£o com demografia e mortalidade induzida pela doen√ßa ===
+dy(1)=mu*n-beta*y(1)*y(2)-mu*y(1);              %Suscet√≠veis      
 dy(2)=beta*y(1)*y(2)-(mu+nu+alpha)*y(2);        %Infectados   
 dy(3)=nu*y(2)-mu*y(3);                          %Recuperados
-% % ============== vers„o do modelo SIR sem demografia =================
-% dy(1)=-beta*y(1)*y(2);              %SuscetÌveis      
+% % ============== vers√£o do modelo SIR sem demografia =================
+% dy(1)=-beta*y(1)*y(2);              %Suscet√≠veis      
 % dy(2)=beta*y(1)*y(2)-nu*y(2);       %Infectados   
 % dy(3)=nu*y(2);                      %Recuperados
